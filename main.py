a197a5910507b8ace7e344d3c7de1e0b3dd27b76
ProstoVanya <efimenkovana6@gmail.com>
2023-11-30 23:44:09 +1000

    commit number

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
 
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     sys.excepthook = except_hook
